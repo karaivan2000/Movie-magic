d70d3f7d936cad5e1d0612319c0ef51946626226
karaivan2000 <133887129+karaivan2000@users.noreply.github.com>
2024-05-27 17:23:42 +0300

    Create  movie action and service

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,7 +1,16 @@
 const router = require(`express`).Router();
 
+const movieService = require(`../services/movieservices`);
+
 router.get(`/create`, (req, res) => {
     res.render(`create`);
+});
+
+router.post(`/create`, (req, res) => {
+    const newMovie = req.body;
+    movieService.create(newMovie);
+    
+    res.send(`Movie should be created`);
 });
 
 module.exports = router;
