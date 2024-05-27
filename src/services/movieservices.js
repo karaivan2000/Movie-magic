47e0fcd5d9eefba0376c1de9fc9276755495ca49
karaivan2000 <133887129+karaivan2000@users.noreply.github.com>
2024-05-27 18:16:13 +0300

    Indicate no movies

--- src/services/movieservices.js
+++ src/services/movieservices.js
@@ -1,12 +1,11 @@
 const movies = [{
     title: 'Home Alone',
-genre: '23ew',
-director: 'Ivam',
-date: '2002',
-imgUrl: '/img/the-little-mermaid.jpg',
-rating: '6',
-description: `The youngest of King Triton's`
-
+    genre: '23ew',
+    director: 'Ivam',
+    date: '2002',
+    imgUrl: '/img/the-little-mermaid.jpg',
+    rating: '6',
+    description: `The youngest of King Triton's`
 }];
 
 exports.getAll = () => {
