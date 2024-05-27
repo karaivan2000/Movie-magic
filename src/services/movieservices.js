d657b8e1740c5a3192b8b92630798132c3c2b16e
karaivan2000 <133887129+karaivan2000@users.noreply.github.com>
2024-05-27 17:51:17 +0300

    Add hardcorded movie

--- src/services/movieservices.js
+++ src/services/movieservices.js
@@ -1,5 +1,15 @@
-const movies = [];
+const movies = [{
+    title: 'Home Alone',
+genre: '23ew',
+director: 'Ivam',
+date: '2002',
+imgUrl: '/img/the-little-mermaid.jpg',
+rating: '6',
+description: `The youngest of King Triton's`
+
+}];
 
 exports.create = (movieData) => {
+    console.log(movieData);
     movies.push(movieData);
 }
